9001b2d06909ebb37df74ffd259c084a9313903f
Kantaaa <63788262+Kantaaa@users.noreply.github.com>
2023-08-15 18:00:50 +0000

    added env

--- tradle-scoreboard/src/utils/firebase.js
+++ tradle-scoreboard/src/utils/firebase.js
@@ -9,15 +9,16 @@ import { getFirestore } from "firebase/firestore";  // <-- Make sure to import t
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDkwLzJqmYHQMArMGQ0rIIk4UlBxs28IYs",
-  authDomain: "tradlescoreboard.firebaseapp.com",
-  databaseURL: "https://tradlescoreboard-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "tradlescoreboard",
-  storageBucket: "tradlescoreboard.appspot.com",
-  messagingSenderId: "251417363256",
-  appId: "1:251417363256:web:d0a9871597d088be36b7cd",
-  measurementId: "G-X3VS1P0JGZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

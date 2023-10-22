68694c38c223fedc1e4f5fd8f890470791ec28ad
Kantaaa <kantasitms1@outlook.com>
2023-10-22 12:37:52 +0200

    Added new navigation button on navbar. Added new component for Rules.

--- tradle-scoreboard/src/App.tsx
+++ tradle-scoreboard/src/App.tsx
@@ -1,118 +1,106 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { db } from './utils/firebase.js';
-import { addDoc, collection, onSnapshot } from 'firebase/firestore';
-import Navbar from './components/Navbar';
-import Scoreboard from './components/Scoreboard';
-import ScoreForm from './components/ScoreForm';
-import { Score } from './types';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { db } from "./utils/firebase.js";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import Navbar from "./components/Navbar";
+import Scoreboard from "./components/Scoreboard";
+import ScoreForm from "./components/ScoreForm";
+import { Score } from "./types";
 import { Stack, ChakraProvider, Button } from "@chakra-ui/react";
-import { assignRanks } from './utils/scoreUtils';
-import { BrowserRouter as Router, Routes, Route, useLocation  } from 'react-router-dom';
-import WeeklyScoreboard from './components/WeeklyScoreboard';
-import MonthlyScoreboard from './components/MonthlyScoreboard';
-
+import { assignRanks } from "./utils/scoreUtils";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import WeeklyScoreboard from "./components/WeeklyScoreboard";
+import MonthlyScoreboard from "./components/MonthlyScoreboard";
 
 import "react-datepicker/dist/react-datepicker.css";
-
 
 const App: React.FC = () => {
   const [scores, setScores] = useState<Score[]>([]);
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
-    
-    const handleSubmitScore = async (newScore: Score) => {
-      try {
-        // Debugging line to print the score object before adding it to Firestore
-        console.log("Adding score:", newScore);
-        
-        await addDoc(collection(db, "scores"), newScore);
-        console.log("Score added successfully");
-      } catch (error) {
-        console.error("Error adding score:", error);
-      }
-    };
-    
-    useEffect(() => {
-        const scoresCollection = collection(db, 'scores');
 
-        const unsubscribe = onSnapshot(scoresCollection, (snapshot) => {
-            const newScores = snapshot.docs.map((doc) => {
-                const data = doc.data() as Score; // Assert the data as Score type
-                return {
-                    id: doc.id,
-                    ...data
-                };
-            });
-            
-            setScores(newScores);
-        });
+  const handleSubmitScore = async (newScore: Score) => {
+    try {
+      // Debugging line to print the score object before adding it to Firestore
+      console.log("Adding score:", newScore);
 
-        return () => {
-            unsubscribe();
+      await addDoc(collection(db, "scores"), newScore);
+      console.log("Score added successfully");
+    } catch (error) {
+      console.error("Error adding score:", error);
+    }
+  };
+
+  useEffect(() => {
+    const scoresCollection = collection(db, "scores");
+
+    const unsubscribe = onSnapshot(scoresCollection, (snapshot) => {
+      const newScores = snapshot.docs.map((doc) => {
+        const data = doc.data() as Score; // Assert the data as Score type
+        return {
+          id: doc.id,
+          ...data,
         };
-    }, []);
+      });
 
-    const filteredScores = scores.filter(score => score.date === selectedDate.toISOString().split('T')[0]);
-    // Sort by attempts in ascending order
-    filteredScores.sort((a, b) => a.attempts - b.attempts || a.name.localeCompare(b.name));
-    const rankedScores = assignRanks(filteredScores);
+      setScores(newScores);
+    });
 
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-  
-    // //Add test data 
-    // const addTestData = () => {
-    //   const players = ['Kevin', 'Alice', 'Bob', 'Charlie', 'David', 'Eve'];
-    //   const days = [0, 1, 2, 3, 4]; // Monday to Friday
-  
-    //   players.forEach((player) => {
-    //     days.forEach((day) => {
-    //       const date = new Date();
-    //       date.setDate(date.getDate() + day);
-    //       const dateString = date.toISOString().split('T')[0];
-          
-    //       const attempts = Math.floor(Math.random() * 6) + 1; // Random attempts between 1 and 6
-    //       const testScore: Score = {
-    //         name: player,
-    //         attempts,
-    //         date: dateString,
-    //         rank: 0
-    //       };
-  
-    //       handleSubmitScore(testScore);
-    //     });
-    //   });
-    // };
-    
-  
+  const filteredScores = scores.filter(
+    (score) => score.date === selectedDate.toISOString().split("T")[0]
+  );
+  // Sort by attempts in ascending order
+  filteredScores.sort(
+    (a, b) => a.attempts - b.attempts || a.name.localeCompare(b.name)
+  );
+  const rankedScores = assignRanks(filteredScores);
 
-    //Main content
+  //Main content
 
-    const MainContent: React.FC<{ onSubmitScore: (newScore: Score) => Promise<void>; rankedScores: Score[] }> = ({ onSubmitScore, rankedScores }) => {
-        const location = useLocation(); 
-      
-        return (
-          <Stack spacing={5}>
-            <Navbar onDateChange={setSelectedDate} />
-            {location.pathname !== '/this-week' && <ScoreForm onSubmitScore={onSubmitScore} />}
-                      {/* <Button onClick={addTestData}>Add Test Data</Button> */}
-                      
-            <Routes>
-              <Route path="/" element={<Scoreboard scores={rankedScores} />} />
-              <Route path="/weekly" element={<WeeklyScoreboard/>} />
-              <Route path="/monthly" element={<MonthlyScoreboard/>} />
-            </Routes>
-          </Stack>
-        );
-      };
+  const MainContent: React.FC<{
+    onSubmitScore: (newScore: Score) => Promise<void>;
+    rankedScores: Score[];
+  }> = ({ onSubmitScore, rankedScores }) => {
+    const location = useLocation();
+
+    return (
+      <Stack spacing={5}>
+        <Navbar onDateChange={setSelectedDate} />
+        {location.pathname !== "/this-week" && (
+          <ScoreForm onSubmitScore={onSubmitScore} />
+        )}
+
+        <Routes>
+          <Route path="/" element={<Scoreboard scores={rankedScores} />} />
+          <Route path="/weekly" element={<WeeklyScoreboard />} />
+          <Route path="/monthly" element={<MonthlyScoreboard />} />
+        </Routes>
+      </Stack>
+    );
+  };
 
 
-      return (
-        <ChakraProvider>
-          <Router>
-            <MainContent onSubmitScore={handleSubmitScore} rankedScores={rankedScores} />
-          </Router>
-        </ChakraProvider>
-      );
-}
+
+  return (
+    <ChakraProvider>
+      <Router>
+        <MainContent
+          onSubmitScore={handleSubmitScore}
+          rankedScores={rankedScores}
+        />
+        
+      </Router>
+    </ChakraProvider>
+  );
+};
 
 export default App;
--- tradle-scoreboard/src/components/Navbar.tsx
+++ tradle-scoreboard/src/components/Navbar.tsx
@@ -1,27 +1,83 @@
-import React from "react";
+import * as React from "react";
+import { useDisclosure } from "@chakra-ui/react";
 import { Flex, Text, Box, Stack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link as rLink } from "react-router-dom";
+import HouseRules from "./HouseRules";
 
 interface NavbarProps {
   onDateChange: (date: Date) => void;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ onDateChange }) => {
-  
+const Navbar: React.FC<NavbarProps> = ({ onDateChange }: NavbarProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex as="nav" justify="space-between" align="center" p={4} bg="#1f1f1f">
-      <Text fontSize="xl" fontWeight="bold" color="white">Tradle Scoreboard 🏆</Text>
       <Stack direction="row" spacing={3}>
-        <Box as={Link} to="/" p={2} borderRadius="md" bg="#333" _hover={{ bg: "#444" }} color={"white"}>
+        <Text fontSize="3xl" fontWeight="bold" color="white">
+          Tradle Scoreboard 🏆
+        </Text>
+        <Box
+          as={rLink}
+          to="/"
+          p={2}
+          borderRadius="md"
+          bg="#333"
+          _hover={{ bg: "#444" }}
+          color={"white"}
+          onClick={() => window.open("https://oec.world/en/tradle/", "_blank")}
+        >
+          Tradle Game🗺️
+        </Box>
+        <Box
+          as={rLink}
+          to="/"
+          p={2}
+          borderRadius="md"
+          bg="#333"
+          _hover={{ bg: "#444" }}
+          color={"white"}
+          onClick={onOpen} 
+        >
+          House Rules📝
+        </Box>
+      </Stack>
+      <Stack direction="row" spacing={3}>
+        <Box
+          as={rLink}
+          to="/"
+          p={2}
+          borderRadius="md"
+          bg="#333"
+          _hover={{ bg: "#444" }}
+          color={"white"}
+        >
           Daily Score
         </Box>
-        <Box as={Link} to="/weekly" p={2} borderRadius="md" bg="#333" _hover={{ bg: "#444" }} color={"white"}>
+        <Box
+          as={rLink}
+          to="/weekly"
+          p={2}
+          borderRadius="md"
+          bg="#333"
+          _hover={{ bg: "#444" }}
+          color={"white"}
+        >
           Weekly Score
         </Box>
-        <Box as={Link} to="/monthly" p={2} borderRadius="md" bg="#333" _hover={{ bg: "#444" }} color={"white"}>
+        <Box
+          as={rLink}
+          to="/monthly"
+          p={2}
+          borderRadius="md"
+          bg="#333"
+          _hover={{ bg: "#444" }}
+          color={"white"}
+        >
           Monthly Score
         </Box>
       </Stack>
+      <HouseRules isOpen={isOpen} onClose={onClose} />
     </Flex>
   );
 };

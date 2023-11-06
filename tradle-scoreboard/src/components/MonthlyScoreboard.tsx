a2a53443c26b374f2e5951b7657cb8ae64327e8b
Kantaaa <kantasitms1@outlook.com>
2023-11-06 21:38:12 +0100

    fix scorebugs and refactoring

--- tradle-scoreboard/src/components/MonthlyScoreboard.tsx
+++ tradle-scoreboard/src/components/MonthlyScoreboard.tsx
@@ -1,9 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { collection, query, where, getDocs } from 'firebase/firestore';
-import { db } from '../utils/firebase';
-import { Score } from '../types';
-import { calculatePoints } from '../utils/scoreUtils';
-import { Table as ChakraTable, Thead, Tbody, Tr, Th, Td, Text, Center, Stack, Button, HStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase";
+import { Score } from "../types";
+import { calculatePoints } from "../utils/scoreUtils";
+import {
+  Table as ChakraTable,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Text,
+  Center,
+  Stack,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
 
 type AggregatedScore = {
   totalAttempts: number;
@@ -18,37 +30,52 @@ const MonthlyScoreboard: React.FC = () => {
 
   const currentDate = new Date();
   currentDate.setMonth(currentDate.getMonth() - monthOffset);
-  const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
-  const currentYear = currentDate.getFullYear();  
+  const currentMonth = currentDate.toLocaleString("default", { month: "long" });
+  const currentYear = currentDate.getFullYear();
 
   useEffect(() => {
     const currentDate = new Date();
     currentDate.setMonth(currentDate.getMonth() - monthOffset);
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth();
-    const scoresRef = collection(db, 'scores');
+    const scoresRef = collection(db, "scores");
     const monthlyScores: Score[] = [];
 
     const fetchScores = async () => {
-      for (let day = 1; day <= 31; day++) {
-        const date = new Date(year, month, day);
-        if (date.getMonth() !== month) break;
-        const dayOfWeek = date.getDay();
-        if (dayOfWeek === 0 || dayOfWeek === 6) continue;
-        const dateString = date.toISOString().split('T')[0];
-        const dayQuery = query(scoresRef, where('date', '==', dateString));
-        const snapshot = await getDocs(dayQuery);
-        snapshot.docs.forEach((doc) => {
-          monthlyScores.push({
-            id: doc.id,
-            ...doc.data() as Score,
-          });
-        });
-      }
-      setMonthlyScores(monthlyScores);
-      console.log("Fetched monthly scores:", monthlyScores);
+      try {
+        const startOfMonth = new Date(Date.UTC(year, month, 1));
+        const endOfMonth = new Date(Date.UTC(year, month + 1, 0));
 
+        // Log the start and end of the month to verify the correct range
+        console.log(
+          `Fetching scores from ${startOfMonth.toISOString()} to ${endOfMonth.toISOString()}`
+        );
+
+        for (let day = 1; day <= endOfMonth.getUTCDate(); day++) {
+          // Use UTC date for comparison
+          const date = new Date(Date.UTC(year, month, day));
+          const dateString = date.toISOString().split("T")[0];
+          const dayQuery = query(scoresRef, where("date", "==", dateString));
+          const snapshot = await getDocs(dayQuery);
+
+          snapshot.docs.forEach((doc) => {
+            const scoreData = doc.data() as Score;
+            const normalizedName = scoreData.name.toUpperCase(); // Normalize the name to upper case
+            monthlyScores.push({
+              id: doc.id,
+              ...scoreData,
+              name: normalizedName, // Use the normalized name
+            });
+          });
+        }
+
+        setMonthlyScores(monthlyScores);
+        console.log("Fetched monthly scores:", monthlyScores);
+      } catch (error) {
+        console.error("Error fetching scores:", error);
+      }
     };
+
     fetchScores();
   }, [monthOffset]);
 
@@ -56,36 +83,46 @@ const MonthlyScoreboard: React.FC = () => {
   monthlyScores.forEach((score) => {
     const normalizedName = score.name.toUpperCase();
     if (aggregatedScores[normalizedName]) {
+      // If so, update the existing entry
       aggregatedScores[normalizedName].totalAttempts += score.attempts;
       aggregatedScores[normalizedName].totalGames += 1;
-      aggregatedScores[normalizedName].gamesPlayed = aggregatedScores[normalizedName].totalGames;
+      aggregatedScores[normalizedName].gamesPlayed =
+        aggregatedScores[normalizedName].totalGames;
     } else {
-      aggregatedScores[normalizedName] = { totalAttempts: score.attempts, totalGames: 1, gamesPlayed: 1 };
+      // If not, create a new entry for this name
+      aggregatedScores[normalizedName] = {
+        totalAttempts: score.attempts,
+        totalGames: 1,
+        gamesPlayed: 1,
+      };
     }
   });
 
   console.log("Aggregated scores:", aggregatedScores);
 
-  const aggregatedScoreArray: Score[] = Object.keys(aggregatedScores).map((name) => {
-    const { totalAttempts, totalGames, gamesPlayed } = aggregatedScores[name];
-    const averageAttempts = totalAttempts / totalGames;
-    const totalPoints = calculatePoints(Math.round(averageAttempts)) * (gamesPlayed ?? 0);
+  const aggregatedScoreArray: Score[] = Object.keys(aggregatedScores).map(
+    (name) => {
+      const { totalAttempts, totalGames, gamesPlayed } = aggregatedScores[name];
+      const averageAttempts = totalAttempts / totalGames;
+      const totalPoints =
+        calculatePoints(Math.round(averageAttempts)) * (gamesPlayed ?? 0);
 
-    return {
-      name,
-      attempts: averageAttempts,
-      rank: 0,
-      date: '',
-      gamesPlayed,
-      totalPoints
-    };
-
-  });
+      return {
+        name,
+        attempts: averageAttempts,
+        rank: 0,
+        date: "",
+        gamesPlayed,
+        totalPoints,
+      };
+    }
+  );
   console.log("Aggregated score array before sorting:", aggregatedScoreArray);
 
-
   // Sort by total points
-  const sortedScores = aggregatedScoreArray.sort((a, b) => (b.totalPoints ?? 0) - (a.totalPoints ?? 0));
+  const sortedScores = aggregatedScoreArray.sort(
+    (a, b) => (b.totalPoints ?? 0) - (a.totalPoints ?? 0)
+  );
 
   // Assign ranks based on totalPoints
   let currentRank = 1;
@@ -101,43 +138,49 @@ const MonthlyScoreboard: React.FC = () => {
   });
   console.log("Sorted and ranked scores:", sortedScores);
 
-
   return (
     <Stack bg="whiteAlpha.600" p={5} borderRadius="md" boxShadow="md">
-       <Center mb={8}>
-        <Text fontSize="3xl" color="black" as={"b"}>Monthly Leaderboard 🏆</Text>
+      <Center mb={8}>
+        <Text fontSize="3xl" color="black" as={"b"}>
+          Monthly Leaderboard 🏆
+        </Text>
       </Center>
       <Center mb={2}>
         <HStack spacing={4}>
-          <Button 
-            width="auto" 
-            onClick={() => setMonthOffset(monthOffset + 1)}
-          >
+          <Button width="auto" onClick={() => setMonthOffset(monthOffset + 1)}>
             Prev Month
           </Button>
 
-          <Button 
-            width="auto" 
-            onClick={() => setMonthOffset(Math.max(0, monthOffset - 1))} 
+          <Button
+            width="auto"
+            onClick={() => setMonthOffset(Math.max(0, monthOffset - 1))}
             disabled={monthOffset === 0}
           >
             Next Month
           </Button>
         </HStack>
       </Center>
-        <Center>
+      <Center>
         <Text fontSize="xl" as={"b"} color="black">
           {currentMonth.toUpperCase()} {currentYear}
         </Text>
-          </Center>  
-      <ChakraTable variant="simple" size="lg" width="80%" m="auto" bg="#333" borderRadius="md" boxShadow="2xl" color="white">
-        
+      </Center>
+      <ChakraTable
+        variant="simple"
+        size="lg"
+        width="80%"
+        m="auto"
+        bg="#333"
+        borderRadius="md"
+        boxShadow="2xl"
+        color="white"
+      >
         <Thead>
           <Tr>
             <Th color="white">Rank</Th>
             <Th color="white">Name</Th>
-            <Th color="white">Total Points</Th> 
-            <Th color="white">Avg. Points per game</Th>
+            <Th color="white">Total Points</Th>
+            <Th color="white">Points/Game</Th>
             <Th color="white">Games Played</Th>
           </Tr>
         </Thead>
@@ -147,7 +190,11 @@ const MonthlyScoreboard: React.FC = () => {
               <Td>{score.rank}</Td>
               <Td>{score.name}</Td>
               <Td>{score.totalPoints}</Td>
-              <Td>{score.totalPoints && score.gamesPlayed ? score.totalPoints/score.gamesPlayed : 0}</Td>
+              <Td>
+                {score.totalPoints && score.gamesPlayed
+                  ? score.totalPoints / score.gamesPlayed
+                  : 0}
+              </Td>
               <Td>{score.gamesPlayed}</Td>
             </Tr>
           ))}
--- tradle-scoreboard/src/utils/firebase.js
+++ tradle-scoreboard/src/utils/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";  
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,16 +16,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-
 };
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-
-
 export { db };
-
